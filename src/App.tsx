6bd834a0c913a23f6a66e462db93e055388568e7
robert <robert.walker6580@gmail.com>
2026-01-01 21:05:27 -0800

    implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Button from './components/Button.tsx';
+import Navbar from './components/Navbar.tsx';
 
 function App() {
   return (
     <main>
-      <h1 className='text-3xl font-semibold'>Home Page</h1>
+      <Navbar />
       <div className='space-x-2'>
         <Button>Default</Button>
         <Button variant={'primary'}>Primary</Button>
