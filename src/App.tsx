d9e731f6e423360d2747f726a370f5a3859021db
robert <robert.walker6580@gmail.com>
2026-01-01 19:46:27 -0800

    Implement button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Button from './components/Button.tsx';
+
 function App() {
   return (
     <main>
-      <h1 className='text-3xl font-semibold text-red-500'>Home Page</h1>
+      <h1 className='text-3xl font-semibold'>Home Page</h1>
+      <div className='space-x-2'>
+        <Button>Default</Button>
+        <Button variant={'primary'}>Primary</Button>
+        <Button variant={'success'}>Success</Button>
+        <Button variant={'info'}>Info</Button>
+        <Button variant={'warning'}>Warning</Button>
+        <Button variant={'destructive'}>Destructive</Button>
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
